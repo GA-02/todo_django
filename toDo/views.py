394a641af7c940aaa01cf275590a9441b2f45499
GA-02 <guzikov.a@mail.ru>
2023-11-03 23:35:01 +0300

    add view on main page

--- toDo/views.py
+++ toDo/views.py
@@ -4,15 +4,24 @@ from django.contrib.auth.decorators import login_required
 import json
 from toDo.models import Task
 
+
 @login_required
 def tasksUser(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().filter(user = request.user)
+    data = []
+    for task in tasks:
+        data.append({'title': task.title, 'priority': task.priority})
     
+    """    
+    tasks = Task.objects.all()
     tasks = tasks.filter(user = request.user)
     data = []
     for task in tasks:
         data.append({'userName': task.user.username, 'title': task.title})
     data2 = json.dumps(data)
     return  HttpResponse(data, content_type='application/json')
+    
+    """
 
+    return render(request, 'index.html', {'tasks': data})
 # Create your views here.
